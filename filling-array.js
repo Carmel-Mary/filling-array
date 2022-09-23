473eaa3aef861ffa42716c5b1de424b49dd39220
carmel-mary ugim-adie <carmy@carmel-marys-MacBook-Pro.local>
2022-09-23 15:23:55 -0400

    filling an array

---
+++ filling-array.js
@@ -0,0 +1,19 @@
+//We want an array, but not just any old array, an array with contents!
+
+////Write a function that produces an array with the numbers 0 to N-1 in it.
+
+//For example, the following code will result in an array containing the numbers 0 to 4:
+
+//arr(5) // => [0,1,2,3,4]
+
+//SOLUTION 1
+function arr(N){ 
+    let reray = []
+    for (let i = 0; i < N; i++){
+       reray.push(i)
+      }
+    return reray
+    
+    }
+
+
